b680e537c387f45c1ae6b20e202421a7118e4130
Navod Dilshan <navoddilshan9@gmail.com>
2022-10-26 03:06:30 +0530

    user class added

--- src/main/java/com/example/assignment/collection/User/User.java
+++ src/main/java/com/example/assignment/collection/User/User.java
@@ -16,13 +16,37 @@ import java.time.LocalDate;
 public class User {
     @Id
     private String userId;
-    private String firstName;
-    private String middleName;
+
+//    personal info
+    private title title;
+    private gender gender;
     private String lastName;
-    private LocalDate birthday;
+    private String initials;
+    private String fullName;
+    private String otherName;
     private String NIC;
+    private String passportNo;
+    private LocalDate birthday;
+    private status status;
+    private Integer noDependents;
+
+//    contact info
+    private String address;
+    private Integer postalCode;
+    private String phoneNumber;
     @NonNull
     private String email;
+
+//    income info
+    private String businessType;
+    private String business;
+    private String officialAddress;
+    private LocalDate empStartDate;
+    private Double MonthlyIncome;
+    private Boolean isTaxPayer;
+
+    //bank usage
+    private Boolean isActivate;
     @NonNull
     private String password;
     @NonNull
